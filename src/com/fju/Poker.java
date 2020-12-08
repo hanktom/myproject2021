12dc1f28614ec14f873e9c544855f6c3655df0fb
Hank Tom <hanktom@gmail.com>
2020-12-08 15:18:23 +0800

    poker game started v2

---
+++ src/com/fju/Poker.java
@@ -0,0 +1,27 @@
+package com.fju;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		PokerCard card = new PokerCard(28);
+		System.out.println(card.value);
+
+		
+		/*
+		int[] cards = new int[52];
+		char[] types = {'S', 'H', 'D', 'C'};
+		for (int i=0; i<52; i++) {
+			cards[i] = i;
+		}
+		for (int i=0; i<52; i++) {
+			if (i%13 == 0 && i!=0) {
+				System.out.println();
+			}
+			char flower = types[cards[i]/13];
+			System.out.print(cards[i]%13+1 + "" + flower + " ");
+			
+		}
+		*/
+	}
+
+}
