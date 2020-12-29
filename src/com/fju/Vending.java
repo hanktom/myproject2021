45295c04cba190639e5a94066c41506bf391670d
Hank Tom <hanktom@gmail.com>
2020-12-29 16:26:59 +0800

    read file/ IO

--- src/com/fju/Vending.java
+++ src/com/fju/Vending.java
@@ -1,11 +1,46 @@
 package com.fju;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Vending {
 
 	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("data.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(isr);
+			String s = br.readLine();
+			System.out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+			
+			/*
+			int n = fis.read();
+			while(n != -1) {
+				System.out.print((char)n);
+				n = fis.read();
+			}
+			*/
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error reading");
+		}
+		
+		/*
 		try {
 			FileReader reader = new FileReader("data1.txt");
 			System.out.println("file opened");
@@ -14,6 +49,7 @@ public class Vending {
 			System.out.println("File not found");
 		}
 		System.out.println("End");
+		*/
 	}
 
 }
